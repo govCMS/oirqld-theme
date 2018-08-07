c8cf370dc8941a675d675e197e923512bf3a2c9d
lnwhsq <38019642+lnwhsq@users.noreply.github.com>
2018-08-08 08:56:50 +1000

    Add files via upload
    Change home banner region & image

--- skeleton/templates/page--front.tpl.php
+++ skeleton/templates/page--front.tpl.php
@@ -1,26 +1,24 @@
 <div class="page_wrapper">
-
 <?php include 'header.tpl.php'; ?>		
-
 	<div class="container">
 		<!--<span class="darkpanel"><span class="overlaytxt">Event text / announcement05.jpg</span></span>-->
 		<div class="row oir_grey">
 			<div class="one-half column">
-			<?php  print render($page['homebannertxt']); ?>	
-			<img src="<?php print path_to_theme(); ?>/images/20151019DT_565v1_sml.jpg" id="" style="max-width:630px;width:100%;height:auto;margin-top: -10px;"></div>
+			<?php  #print render($page['homebannertxt']); ?>	
+			<!--<img src="<?php print path_to_theme(); ?>/images/20151019DT_565v1_sml.jpg" id="" style="max-width:630px;width:100%;height:auto;margin-top: -10px;">--></div>
 			<div class="one-half column sm_padding">
-			<?php print render($page['homebanner']); ?>
+			<?php #print render($page['homebanner']); ?>
 			</div>
-		<!--	
-		<div class="twelve columns">
-		<img src="<?php print path_to_theme(); ?>/images/20151019DT_565v1_med.jpg" id="" style="max-width:1140px;width:100%;height:auto;margin-top: -10px;">
 		</div>
-			-->
-			
-			
-		</div>
-	</div>	
-
+		<div class="row">
+			<div class="twelve columns">
+<img src="<?php print path_to_theme(); ?>/images/5939_Labourhire_WEB_image.jpg" id="" style="max-width:1140px;width:100%;height:auto;margin-top: -10px;"> 
+<!-- <img src="<?php print path_to_theme(); ?>/images/678500738-2048x2048.jpg" id="" style="max-width:1140px;width:100%;height:auto;margin-top: -10px;"> -->
+<!-- <img src="<?php print path_to_theme(); ?>/images/935776976-2048x2048.jpg" id="" style="max-width:1140px;width:100%;height:auto;margin-top: -10px;"> -->
+<!-- <img src="<?php print path_to_theme(); ?>/images/oir-banner-02.jpg" id="" style="max-width:1140px;width:100%;height:auto;margin-top: -10px;"> -->
+		</div>	
+	</div>
+</div>	
 	<?php print render($page['featured']); ?>
 
 	
